348f4ecda97ed36e64fb3a85847addf3e6ff50d3
keerthiDeveloper <keerthifullstackdeveloper@gmail.com>
2020-10-15 02:09:31 +0530

    error fix

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
+  isOpen: boolean;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
